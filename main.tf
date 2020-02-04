ebe1fca0a537b0ccfcd6b34ec240418a1635f0ad
Rune Rønneseth <rune.ronneseth@aiqu.no>
2020-02-04 16:07:43 +0100

    Running fmt..

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ locals {
   # AKS settings used by kubernetes provider internally (See versions.tf)
   # Used since we have 2 types of clusters to select correct output, and the
   # output kube_config values/hash can't be set to null
-  aks_kube_config    = !(var.initialized) ? null : var.cluster_type == "advanced" ? module.aks_advanced.kube_admin_config[0]                                      : module.aks_basic.kube_config[0]
-  aks_host           = !(var.initialized) ? ""   : var.cluster_type == "advanced" ? module.aks_advanced.kube_admin_config[0].host                                 : module.aks_basic.kube_config[0].host
-  aks_certificate    = !(var.initialized) ? ""   : var.cluster_type == "advanced" ? base64decode(module.aks_advanced.kube_admin_config[0].client_certificate)     : base64decode(module.aks_basic.kube_config[0].client_certificate)
-  aks_key            = !(var.initialized) ? ""   : var.cluster_type == "advanced" ? base64decode(module.aks_advanced.kube_admin_config[0].client_key)             : base64decode(module.aks_basic.kube_config[0].client_key)
-  aks_ca_certificate = !(var.initialized) ? ""   : var.cluster_type == "advanced" ? base64decode(module.aks_advanced.kube_admin_config[0].cluster_ca_certificate) : base64decode(module.aks_basic.kube_config[0].cluster_ca_certificate)
+  aks_kube_config    = ! (var.initialized) ? null : var.cluster_type == "advanced" ? module.aks_advanced.kube_admin_config[0] : module.aks_basic.kube_config[0]
+  aks_host           = ! (var.initialized) ? "" : var.cluster_type == "advanced" ? module.aks_advanced.kube_admin_config[0].host : module.aks_basic.kube_config[0].host
+  aks_certificate    = ! (var.initialized) ? "" : var.cluster_type == "advanced" ? base64decode(module.aks_advanced.kube_admin_config[0].client_certificate) : base64decode(module.aks_basic.kube_config[0].client_certificate)
+  aks_key            = ! (var.initialized) ? "" : var.cluster_type == "advanced" ? base64decode(module.aks_advanced.kube_admin_config[0].client_key) : base64decode(module.aks_basic.kube_config[0].client_key)
+  aks_ca_certificate = ! (var.initialized) ? "" : var.cluster_type == "advanced" ? base64decode(module.aks_advanced.kube_admin_config[0].cluster_ca_certificate) : base64decode(module.aks_basic.kube_config[0].cluster_ca_certificate)
 }
 
 # ==============================================================================
@@ -27,7 +27,7 @@ module "resource_group" {
 
 # Generate private/public keys if public key is not provided
 module "ssh_keys" {
-  source = "./modules/ssh-keys"
+  source  = "./modules/ssh-keys"
   enabled = (var.ssh_public_key != "") ? false : true
 }
 
@@ -65,9 +65,9 @@ module "log_analytics_solution" {
 
 # Create basic Azure AD application
 module "service_principals_basic" {
-  source         = "./modules/aad-basic"
-  enabled        = (var.cluster_type == "basic") ? true : false
-  prefix         = var.prefix
+  source  = "./modules/aad-basic"
+  enabled = (var.cluster_type == "basic") ? true : false
+  prefix  = var.prefix
 }
 
 module "aks_basic" {
@@ -76,9 +76,9 @@ module "aks_basic" {
   prefix              = var.prefix
   resource_group_name = var.resource_group_override != "" ? var.resource_group_override : module.resource_group.name
   location            = var.location
-  client_id           = var.appid_client            != "" ? var.appid_client            : module.service_principals_basic.client_id
-  client_secret       = var.spid_client_secret      != "" ? var.spid_client_secret      : module.service_principals_basic.client_secret
-  ssh_public_key      = var.ssh_public_key          != "" ? var.ssh_public_key          : module.ssh_keys.public_ssh_key
+  client_id           = var.appid_client != "" ? var.appid_client : module.service_principals_basic.client_id
+  client_secret       = var.spid_client_secret != "" ? var.spid_client_secret : module.service_principals_basic.client_secret
+  ssh_public_key      = var.ssh_public_key != "" ? var.ssh_public_key : module.ssh_keys.public_ssh_key
   admin_user          = var.admin_user
   kube_version        = var.kube_version
   tags                = var.tags
@@ -112,11 +112,11 @@ module "aks_advanced" {
   prefix              = var.prefix
   location            = var.location
   resource_group_name = var.resource_group_override != "" ? var.resource_group_override : module.resource_group.name
-  appid_server        = var.appid_server            != "" ? var.appid_server            : module.service_principals_rbac.appid_server
-  appid_client        = var.appid_client            != "" ? var.appid_client            : module.service_principals_rbac.appid_client
-  spid_server_secret  = var.spid_server_secret      != "" ? var.spid_server_secret      : module.service_principals_rbac.spid_server_secret
-  spid_client_secret  = var.spid_client_secret      != "" ? var.spid_client_secret      : module.service_principals_rbac.spid_client_secret
-  ssh_public_key      = var.ssh_public_key          != "" ? var.ssh_public_key          : module.ssh_keys.public_ssh_key
+  appid_server        = var.appid_server != "" ? var.appid_server : module.service_principals_rbac.appid_server
+  appid_client        = var.appid_client != "" ? var.appid_client : module.service_principals_rbac.appid_client
+  spid_server_secret  = var.spid_server_secret != "" ? var.spid_server_secret : module.service_principals_rbac.spid_server_secret
+  spid_client_secret  = var.spid_client_secret != "" ? var.spid_client_secret : module.service_principals_rbac.spid_client_secret
+  ssh_public_key      = var.ssh_public_key != "" ? var.ssh_public_key : module.ssh_keys.public_ssh_key
   admin_user          = var.admin_user
   kube_version        = var.kube_version
   tags                = var.tags
@@ -139,7 +139,7 @@ module "aks_advanced" {
 
 module "kube_management" {
   source             = "./modules/kube-modules"
-  enabled            = (var.initialized && var.kube_management_enabled )? true : false
+  enabled            = (var.initialized && var.kube_management_enabled) ? true : false
   admin_group        = var.kube_admin_group
   dashboard_as_admin = var.kube_dashboard_as_admin
 }
